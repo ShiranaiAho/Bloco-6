554917b84fb613784377001fa8ec12a975cd5f0f
Alexandre Fogaça <alexandr.fogaca@gmail.com>
2020-08-04 18:15:53 -0300

    Exercicios do site 1

--- exercises/6_1/script.js
+++ exercises/6_1/script.js
@@ -1,5 +1,5 @@
-const submitBtn = document.querySelector('#submit');
-const name = document.querySelector('#name');
+const formulario = document.querySelector('#formulario');
+const nome = document.querySelector('#name');
 const email = document.querySelector('#email');
 const cpf = document.querySelector('#cpf');
 const adress = document.querySelector('#adress');
@@ -10,10 +10,15 @@ const date = document.querySelector('#date');
 const curriculum = document.querySelector('#curriculum');
 const state = document.querySelector('#state');
 
-submitBtn.addEventListener('click', function (event) {
-  curriculum.innerHTML = name.value;
-  // inserir o resto dos valores, além dos titulos do conteudo.
+formulario.addEventListener('submit', function (event) {
   event.preventDefault();
+
+  const formElements = formulario.elements;
+  for (i in formElements) {
+    if (formElements[i].tagName != 'FIELDSET' && formElements[i].tagName != 'BUTTON' && formElements[i].tagName != undefined) {
+      curriculum.innerHTML += formElements[i].value + "; ";
+    } 
+  }
 });
 
 const stateArray = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins']
@@ -22,7 +27,7 @@ window.onload = function () {
   for (i in stateArray) {
     let element = document.createElement('option');
     element.innerHTML = stateArray[i];
-    element.value = [i];
+    element.value = stateArray[i];
     state.appendChild(element);
   }
-}
+};
